0947bd7d8e4e99f88641b7ddc723451e55044eae
Eunhae Hwang <ehwang1@student.cvtc.edu>
2022-10-27 00:08:50 -0500

    [finishes #183645024] Validate the technology stack

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ require("dotenv").config(); // Read environment variables from .env
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5163;
+const { Pool } = require("pg");
+
+const pool = new Pool({
+ connectionString: process.env.DATABASE_URL, 
+ ssl: {
+  rejectUnauthorized: false
+  }
+});
 
 express()
  .use(express.static(path.join(__dirname, "public")))
@@ -10,6 +18,31 @@ express()
  .set("views", path.join(__dirname, "views"))
  .set("view engine", "ejs")
  .get("/", async(req, res) => {
+  try {
+    const client = await pool.connect();
+    const buttonSql = "SELECT * FROM buttons ORDER By id ASC;";
+    const buttons = await client.query(buttonSql);
+    const args = {
+     "buttons": buttons ? buttons.rows : null
+};
+res.render("pages/index", args); 
+} 
+catch (err) {
+  console.error(err);
+  res.set({
+  "Content-Type": "application/json"
+  });
+  res.json({
+   error: err
+   });
+}
+})
+ .post("/log", async(req, res) => {
+  res.set({
+  "Content-Type": "application/json"
+});
+
+
   const args = {
   time: Date.now()
 };
@@ -19,9 +52,31 @@ express()
   res.set({
   "Content-Type": "application/json"
 });
+
+try {
+  const client = await.pool.connect();
+  const id = req.body.id;
+  const insertSql = `INSERT INTO buttons (name)
+    VALUES (concat(`Child of `, $1))
+    RETURNING id AS new_id;`;
+  const selectSql = "SELECT LOCALTIME;";
+
+  const insert = await client.query(insertSql, [id]);
+  const select = await client.query(selectSql);
+
+  const response = {
+   newId: insert ? insert.rows[0] : null,
+   when: select ? select.rows[0] : null
+};
+res.json(response);
+client.reslease();
+}
+catch (err) {
+console.error(err);
 res.json({
-time: Date.now()
+error: err
 });
+}
 })
  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
